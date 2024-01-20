c5bf1e89f26bc08e92e78f0af7904251ff8a5452
kitou88C39 <kanfutrooper@icloud.com>
2024-01-20 15:14:31 +0900

    I made a crawler and collected car parts.

--- car_spider.py
+++ car_spider.py
@@ -9,3 +9,6 @@ option = webdriver.ChromeOptions()
 option.add_argument('__headeless')
 option.add_argument('__disable_gpu')
 option.add_argument('__lang_ja')
+broeser = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
+df = pd.DataFrame(colums=['name', 'image', 'price', 'category','car'])
+url = 'https://motorz-garage.com/parts/'
