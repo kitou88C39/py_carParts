ff2854934993688086e426872a6081b91f569626
kitou88C39 <kanfutrooper@icloud.com>
2024-01-23 16:31:08 +0900

    Beatifulsoupの使い方

--- scrapy_tutorial/step/3-8.py
+++ scrapy_tutorial/step/3-8.py
@@ -3,3 +3,8 @@ import requests
 # 画像ファイルを取得
 image_url="https://creaters-you.com/pc.png"
 imgdata=requests.get(image_url)
+# URLから最後のファイル名を取り出す
+filename=image_url.split("/")[-1]
+# 画像データをファイルに書き出す
+with open(filename, mode="wb")as f:
+    f.write(imgdata.content)
