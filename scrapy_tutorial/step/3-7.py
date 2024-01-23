0453851c8d4f0185525197180919bbe566a006c8
kitou88C39 <kanfutrooper@icloud.com>
2024-01-23 16:00:33 +0900

    Beatifulsoupの使い方

---
+++ scrapy_tutorial/step/3-7.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeatifulSoup
+import urllib
+
+load_url="https://creaters-you.com/"
+html=requests.get(load_url)
+soup=BeatifulSoup(html.content,"html.parser")
+
+# ファイルを書き込みモードで開く
+filename="linklist.txt"
+with open(filename, "w") as f:
+
+
+
+# 全てのaタグを検索 リンクを表示
+for element in soup.find_all("a"):
+    print(element.text)
+    url=element.get("href")
+    link_url=urllib.parser.urljoin(load_url, url)
+    print(link_url)
