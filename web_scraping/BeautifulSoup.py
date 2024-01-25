7c35b97989c81fa40155bd17b5024cd25e3af728
kitou88C39 <kanfutrooper@icloud.com>
2024-01-25 11:38:50 +0900

    Pandas scraping

--- web_scraping/BeautifulSoup.py
+++ web_scraping/BeautifulSoup.py
@@ -3,3 +3,7 @@ import pandas as pd
 url = "https://fainance.yahoo.com/"
 data = pd.read_html(url, header=0)
 data[0].head()
+data[0].tail()
+data[0]["Adj Close**"]=pd.to_numeric(data[0]["Adj Close**"],errors="coerce")
+data[0].tail()
+data[0].dropna（inplace=True）
