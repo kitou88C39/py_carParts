c1b8f0c4b7d1082db7e50dddfaded0e773db2b9a
kitou88C39 <kanfutrooper@icloud.com>
2024-01-26 15:03:33 +0900

    selenium

--- web_scraping/Selenium2.py
+++ web_scraping/Selenium2.py
@@ -1,9 +1,18 @@
 from selenium from webdriver
 from time import sleep
 
+# ログインする方法
 USERNAME = "yoshitaka"
 PASSWORD = "password"
 driver = webdriver.Chrome("chromedriver")
 target_url="https://www.instagram.com/"
 driver.get(target_url)
 sleep(3)
+
+# ログイン画面でユーザーネーム・パスワードの入力
+error_flg = False
+try:
+    username_input = driver.find_element_by_xpath("//input[@aria-label='電話番号、ユーザーネーム、メールアドレス']")
+except Exception:
+    error_flg = True
+    print("ユーザー名、パスワード入力時にエラーが発生しました")
