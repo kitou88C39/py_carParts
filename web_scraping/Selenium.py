90b65bb6cc117d31d05cd5f4edd4f7c2c4ff44b0
kitou88C39 <kanfutrooper@icloud.com>
2024-01-26 14:12:42 +0900

    selenium

--- web_scraping/Selenium.py
+++ web_scraping/Selenium.py
@@ -21,6 +21,7 @@ csv_header = ["検索順位","タイトル","URL"]
 writer.writerow(csv_header)
 
 i = 0
+ranking = 1
 while True:
     i = i + 1
     seleep(1)
@@ -28,7 +29,8 @@ for elem_h3 driver.find_element_by_path("//a/h3"):
     print(elem_h3.text)
     elem_a = elem_h3.find_element_by_path("..")
     print(elem_a.get_attribute("href"))
-    next_link = driver find_element_by_id("pnnext")
-    driver.get(next_link.get_attribute("href"))
-    if i > 4:
-        break
+    ranking = ranking + 1
+next_link = driver find_element_by_id("pnnext")
+driver.get(next_link.get_attribute("href"))
+if i > 4:
+    break
