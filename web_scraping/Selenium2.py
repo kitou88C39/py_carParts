4ddb914f86efb910ec14646a4a821d3f84c60aa6
kitou88C39 <kanfutrooper@icloud.com>
2024-01-26 14:52:44 +0900

    selenium

--- web_scraping/Selenium2.py
+++ web_scraping/Selenium2.py
@@ -3,3 +3,7 @@ from time import sleep
 
 USERNAME = "yoshitaka"
 PASSWORD = "password"
+driver = webdriver.Chrome("chromedriver")
+target_url="https://www.instagram.com/"
+driver.get(target_url)
+sleep(3)
