4c0161b5640c3fdec5725239b5b3e12d673881c3
kitou88C39 <kanfutrooper@icloud.com>
2024-01-27 14:22:34 +0900

    selenium

--- using_selenium/Selenium.py
+++ using_selenium/Selenium.py
@@ -10,3 +10,10 @@ browser = webdriver.Chrome()
 browser.get("https//scraping-for-beginner.herokuapp.com/login_page")
 # 要素を指定
 elem_username = browser.find_element_by_id('username')
+elem_username.send_keys('imanishi')
+
+elem_password = browser.find_element_by_id('password')
+elem_password.send_keys('kohei')
+
+elem_login_btn = browser.find_element_by_id('login-btn')
+elem_login_btn.click()
